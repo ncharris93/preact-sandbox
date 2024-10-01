6692653d77d5bb73365e20da69beecc74c51ed9c
Noah Harris <ncharris93@gmail.com>
2024-10-01 11:24:16 -0400

    feat: add header ux logic around the height of the header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,20 @@
-import { useLocation } from 'preact-iso';
+import { useLocation } from "preact-iso";
+import { AppConfig } from "../config";
 
 export function Header() {
-	const { url } = useLocation();
+  const { url } = useLocation();
+  const { isNativeMobile } = AppConfig;
 
-	return (
-		<header>
-			<nav>
-				<a href="/" class={url == '/' && 'active'}>
-					Home
-				</a>
-				<a href="/404" class={url == '/404' && 'active'}>
-					404
-				</a>
-			</nav>
-		</header>
-	);
+  return (
+    <header style={isNativeMobile && { paddingTop: 40 }}>
+      <nav>
+        <a href="/" class={url == "/" && "active"}>
+          Home
+        </a>
+        <a href="/404" class={url == "/404" && "active"}>
+          404
+        </a>
+      </nav>
+    </header>
+  );
 }
